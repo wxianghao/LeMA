3226f73b3276787a2f9ee9ef9b1fbaad74df3355
xianghao <wang.xianghao@outlook.com>
2026-01-02 19:08:17 +1100

    add test-simple

---
+++ experiments/test-simple/run_nsys.sh
@@ -0,0 +1,2 @@
+module load nvhpc-profilers/24.7 
+nsys profile -f true -o test_simple python test_simple.py 
---
+++ experiments/test-simple/test_simple.py
@@ -0,0 +1,37 @@
+from lema import LeMA
+import torch
+import nvtx
+
+class SimpleDense(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear_tanh_stack = torch.nn.Sequential(
+            torch.nn.Linear(784, 512), torch.nn.Tanh(), torch.nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        return self.linear_tanh_stack(x)
+
+
+# Get devices
+device_count = torch.cuda.device_count()
+devices = [torch.device(f"cuda:{i}") for i in range(device_count)]
+
+# Prepare data
+x = torch.randn(20000, 784, pin_memory=True)
+y = torch.randn(20000, 10, pin_memory=True)
+
+# Create model
+residual_fn = lambda a, b: torch.sum(a - b, axis=1).flatten()
+model = SimpleDense()
+lema_optim = LeMA(model, devices, residual_fn)
+for device in devices:
+    torch.cuda.synchronize(device)
+
+# Test multiple steps
+# start_event = torch.cuda.Event()
+# end_event = torch.cuda.Event()
+for _ in range(2):
+    # start_event.record()
+    lema_optim.step(x, y)
+    # end_event.record()
