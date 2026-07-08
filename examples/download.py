06e42f095a6a1aba94928035685975bba007616e
xianghao <wxianghao@outlook.com>
2026-07-08 15:39:33 +0000

    fix output folder path of download.py

--- examples/download.py
+++ examples/download.py
@@ -1,5 +1,5 @@
 import torchvision
 
-root_path = "./data"
+root_path = "./.data"
 trainset = torchvision.datasets.MNIST(root_path, train=True, download=True)
 testset = torchvision.datasets.MNIST(root_path, train=False)
